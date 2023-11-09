cd9ee139cffddd497e76560c8b898d19c4e8f2ab
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-11-09 23:06:40 +0600

    added for loop to fill the window with the grass texture (only horizontal at the moment)

--- Main.java
+++ Main.java
@@ -8,8 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
         // Загрузка изображения
         imageIcon = new ImageIcon("grass.jpg");
 
@@ -19,39 +17,27 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Создание компонента для отображения изображения
-        JLabel label = new JLabel(imageIcon);
-        JLabel label1 = new JLabel(imageIcon);
-        label.setBounds(0, 0, 100, 100); // Установка координат и размеров метки
-        label1.setBounds(200, 0, 100, 100); // Установка координат и размеров метки
+        // JLabel label = new JLabel(imageIcon);
+        // JLabel label1 = new JLabel(imageIcon);
+        // label.setBounds(0, 0, 100, 100); // Установка координат и размеров метки
+        // label1.setBounds(200, 0, 100, 100); // Установка координат и размеров метки
+
+        for (int x = 0; x < 800; x += 100) {
+            JLabel label = new JLabel(imageIcon);
+            label.setBounds(x, 0, 100, 100); // Установка координат и размеров метки
+            // Добавление компонента на окно
+            frame.add(label);
+        }
 
         // Добавление компонента на окно
-        frame.add(label);
-        frame.add(label1);
+        // frame.add(label);
+        // frame.add(label1);
 
         // Установка размеров окна
         frame.setSize(800, 600);
 
         // Отображение окна
         frame.setVisible(true);
-
-        // // Create a JFrame object
-        // JFrame frame = new JFrame("самая полезная в мире программа");
-
-        // // Загрузка изображения
-        // image = Toolkit.getDefaultToolkit().getImage("grass.jpg");
-
-        // // Использование изображения
-        // // Пример: отображение изображения на панели
-        // MyPanel panel = new MyPanel(image);
-        // panel.repaint();
-
-        // frame.setSize(800, 600);
-
-        // frame.add(panel);
-
-        // // Make the window visible
-        // frame.setVisible(true);
-        // frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     }
 }
