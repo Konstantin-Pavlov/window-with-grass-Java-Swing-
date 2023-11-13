413f5069a79d3ffe8285fd92df71a1e5d011e209
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-11-13 15:51:14 +0600

    code reformat

--- Main.java
+++ Main.java
@@ -1,44 +1,38 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
 
-    private static Image image;
-    private static ImageIcon imageIcon;
-
     public static void main(String[] args) {
-
         // Загрузка изображения
-        imageIcon = new ImageIcon("grass.jpg");
+        ImageIcon imageIcon = new ImageIcon("grass.jpg");
 
         // Создание окна
         JFrame frame = new JFrame("Grass texture");
         frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Создание компонента для отображения изображения
-        // JLabel label = new JLabel(imageIcon);
-        // JLabel label1 = new JLabel(imageIcon);
-        // label.setBounds(0, 0, 100, 100); // Установка координат и размеров метки
-        // label1.setBounds(200, 0, 100, 100); // Установка координат и размеров метки
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         for (int y = 0; y < 600; y += 100) {
             for (int x = 0; x < 800; x += 100) {
-                JLabel label = new JLabel(imageIcon);
+                JLabel label = new JLabel(imageIcon); // Создание компонента для отображения изображения
                 label.setBounds(x, y, 100, 100); // Установка координат и размеров метки
-                frame.add(label);// Добавление компонента на окно
+                frame.add(label); // Добавление компонента на окно
             }
         }
 
-        // Добавление компонента на окно
-        // frame.add(label);
-        // frame.add(label1);
+        frame.setSize(800, 600); // Установка размеров окна
 
-        // Установка размеров окна
-        frame.setSize(800, 600);
-
-        // Отображение окна
-        frame.setVisible(true);
-
+        frame.setVisible(true); // Отображение окна
     }
+
+    // private static void centerFrameOnScreen(JFrame frame) {
+    // // Get the size of the screen
+    // Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    // // Calculate the position to center the frame
+    // int x = (screenSize.width - frame.getWidth()) / 2;
+    // int y = (screenSize.height - frame.getHeight()) / 2;
+
+    // // Set the frame's location
+    // frame.setLocation(x, y);
+    // }
 }
